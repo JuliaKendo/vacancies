f50de17a25da4deed7aa93a0ce22b8f71a613ec0
JuliaKendo <50887767+JuliaKendo@users.noreply.github.com>
2019-06-23 20:13:43 +0300

    Update fetch_hh.py

--- fetch_hh.py
+++ fetch_hh.py
@@ -6,37 +6,6 @@ LIST_LANGUAGES = ("JavaScript","Java","Python","Ruby","Swift","1С")
 URL_TEMPLATE = "https://api.hh.ru/vacancies"
 TABLE_DATA = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
 
-def get_predict_salary(salary_from, salary_to):
-    result = None
-    if salary_from > 0 and salary_to > 0:
-        result = (salary_from + salary_to) / 2
-    elif salary_from > 0 and salary_to == 0:
-        result = salary_from * 1.2
-    elif salary_from == 0 and salary_to > 0:
-        result = salary_to * 0.8
-    else:
-        result = 0
-    return result    
-
-
-def get_predict_rub_salary_hh(vacancy):
-
-    result = 0
-    if vacancy['from'] is None:
-        salary_from = 0 
-    else:
-        salary_from = vacancy['from']
-    if vacancy['to'] is None:
-        salary_to = 0 
-    else:
-        salary_to = vacancy['to']
-    if vacancy['currency'] is None:
-        return result
-    elif vacancy['currency'] != 'RUR':
-        return result
-    result = get_predict_salary(salary_from, salary_to)
-    return result     
-    
 def fetch_records(prog_language):
     pages_list = []
     params = {'area':'1',
@@ -54,62 +23,64 @@ def fetch_records(prog_language):
             break
     return pages_list
 
-def get_vacancy_info(found_vacancies):
-    vacancy_info = {}
-    list_salary = []
-    vacancy_info['vacancies_found'] = found_vacancies['found']
-    for vacancy in found_vacancies['items']:
-        if vacancy['salary']:
-            salary_of_vacancy = get_predict_rub_salary_hh(vacancy['salary'])
-            if salary_of_vacancy > 0:
-                list_salary.append(salary_of_vacancy)
+def get_predict_rub_salary_hh(vacancy):
 
-    vacancy_info['vacancies_processed'] = len(list_salary)
-    vacancy_info['sum_salary'] = sum(list_salary)
+    salary_from = 0
+    salary_to = 0
+    if vacancy is None:
+        return [0, 0]
+    if vacancy['from'] is not None:
+        salary_from = vacancy['from']
+    if vacancy['to'] is not None:
+        salary_to = vacancy['to']
+    if vacancy['currency'] is None:
+        return [0, 0]
+    elif vacancy['currency'] != 'RUR':
+        return [0, 0]
+    return [salary_from, salary_to]
 
-    return vacancy_info
-
-def add_vacancy_info(info_vacancies, block_vacancy_info):
-    info_vacancies['vacancies_found'] += block_vacancy_info['vacancies_found']
-    info_vacancies['vacancies_processed'] += block_vacancy_info['vacancies_processed']
-    info_vacancies['sum_salary'] += block_vacancy_info['sum_salary']
-
-def get_vacancies_by_lang(prog_language):
-    info_vacancies_by_lang = {'vacancies_found':0,'vacancies_processed':0,'sum_salary':0}
-    vacancy_records = fetch_records(prog_language)
-    for vacancy_record in vacancy_records:
-        block_vacancy_info = get_vacancy_info(vacancy_record)
-        add_vacancy_info(info_vacancies_by_lang, block_vacancy_info)
-    return info_vacancies_by_lang
+def get_predict_salary(predict_salary):
+    result = None
+    salary_from = predict_salary[0] 
+    salary_to = predict_salary[1]
+    if salary_from > 0 and salary_to > 0:
+        result = (salary_from + salary_to) / 2
+    elif salary_from > 0 and salary_to == 0:
+        result = salary_from * 1.2
+    elif salary_from == 0 and salary_to > 0:
+        result = salary_to * 0.8
+    else:
+        result = 0
+    return result    
 
 def fetch_hh():
-    vacancies = {}
-    
+
     for prog_language in LIST_LANGUAGES:
+        info_vacancies = {'vacancies_found':0,'vacancies_processed':0,'sum_salary':0}
 
-        info_vacancies = {'vacancies_found':0,'vacancies_processed':0,'average_salary':0}        
-        info_vacancies_by_lang = get_vacancies_by_lang(prog_language)
-        info_vacancies['vacancies_found'] = "{}".format(info_vacancies_by_lang['vacancies_found'])
-        info_vacancies['vacancies_processed'] = "{}".format(info_vacancies_by_lang['vacancies_processed'])
+        pages_list = fetch_records(prog_language)
+        for page_vacancies in pages_list:
+            info_vacancies['vacancies_found'] += page_vacancies['found']
+            for vacancy in page_vacancies['items']:
+                predict_rub_salary_hh = get_predict_rub_salary_hh(vacancy['salary'])
+                predict_salary = get_predict_salary(predict_rub_salary_hh)
+                if predict_salary > 0:
+                    info_vacancies['vacancies_processed'] += 1
+                    info_vacancies['sum_salary'] += predict_salary
+
         try:
-            average_salary = info_vacancies_by_lang['sum_salary'] / info_vacancies_by_lang['vacancies_processed']
+            average_salary = int(info_vacancies['sum_salary'] / info_vacancies['vacancies_processed'])
         except ZeroDivisionError:
-            average_salary = 0    
-        info_vacancies['average_salary'] = "{}".format(int(average_salary))
-        vacancies[prog_language] = info_vacancies
+            average_salary = 0
 
-    return vacancies
+        total_by_language = [prog_language, 
+                             info_vacancies['vacancies_found'], 
+                             info_vacancies['vacancies_processed'], 
+                             average_salary] 
 
-def get_vacancies_hh():
-    vacancies_hh = fetch_hh()
-    for prog_language in vacancies_hh:
-        vacancies_info = vacancies_hh[prog_language]
-        string_of_table = [prog_language, 
-                           vacancies_info['vacancies_found'], 
-                           vacancies_info['vacancies_processed'], 
-                           vacancies_info['average_salary']]
-        TABLE_DATA.append(string_of_table)
+        TABLE_DATA.append(total_by_language)  
 
     table_instance = AsciiTable(TABLE_DATA, 'HeadHanter Moscow')
     table_instance.justify_columns[3] = 'right'
     return table_instance.table
+      
